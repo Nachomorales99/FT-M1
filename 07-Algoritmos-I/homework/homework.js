61a92c735761ec193f3e17abd871c09e7c23c1b7
Nacho Morales <nacho.morales5@gmail.com>
2023-01-26 01:39:01 -0300

    Henry dia 7

--- 07-Algoritmos-I/homework/homework.js
+++ 07-Algoritmos-I/homework/homework.js
@@ -1,12 +1,23 @@
 'use strict'
 // No cambies los nombres de las funciones.
 
-function factorear(num, factores = [1]) {
+function factorear(num) {
   // Factorear el número recibido como parámetro y devolver en un array
   // los factores por los cuales se va dividiendo a dicho número (De menor a mayor)
   // Ej: factorear(180) --> [1, 2, 2, 3, 3, 5] Ya que 1x2x2x3x3x5 = 180 y son todos números primos
   // Tu código:
 
+  let factores = [1];
+
+  for (let i = 2; i <= num; i++) {
+    while (num % i == 0) {
+      factores.push(i);
+      num = num / i;
+      console.log(num)
+    }
+  }
+
+  return factores;
 }
 
 function bubbleSort(array) {
@@ -26,7 +37,7 @@ function bubbleSort(array) {
     }
   }
   return array;
-}; 
+};
 
 function insertionSort(array) {
   // Implementar el método conocido como insertionSort para ordenar de menor a mayor
@@ -34,8 +45,17 @@ function insertionSort(array) {
   // Devolver el array ordenado resultante
   // Tu código:
 
+  for (let i = 1; i < array.length; i++) {
+    let aux = array[i];
+    let j = i - 1;
+    while (j >= 0 && array[j] > aux) {
+      array[j + 1] = array[j];
+      j--;
+    }
+    array[j + 1] = aux;
+  }
+  return array;
 }
-
 
 function selectionSort(array) {
   // Implementar el método conocido como selectionSort para ordenar de menor a mayor
@@ -43,8 +63,23 @@ function selectionSort(array) {
   // Devolver el array ordenado resultante
   // Tu código:
 
-}
+  for (let i = 0; i < array.length; i++) {
+    let min = i;
+    for (let j = i + 1; j < array.length; j++) {
+      if (array[min] > array[j]) {
+        min = j;
+      }
+    }
 
+    let aux = array[i];
+    array[i] = array[min];
+    array[min] = aux;
+  }
+  return array;
+
+} 
+
+console.log(selectionSort([5,1,2,4,8]))
 
 // No modificar nada debajo de esta línea
 // --------------------------------
